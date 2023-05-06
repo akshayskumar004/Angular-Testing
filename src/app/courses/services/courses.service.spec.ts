11f8db0d91c313a1860e0099d88a9d8245d4d645
akshayskumar004 <akshayskumar004@gmail.com>
2023-05-06 21:56:53 +0530

    y

--- src/app/courses/services/courses.service.spec.ts
+++ src/app/courses/services/courses.service.spec.ts
@@ -4,6 +4,7 @@ import {
   HttpClientTestingModule,
   HttpTestingController,
 } from "@angular/common/http/testing";
+import { COURSES } from "../../../../server/db-data";
 
 describe("CoursesService", () => {
   let coursesService: CoursesService,
@@ -16,5 +17,15 @@ describe("CoursesService", () => {
     coursesService = TestBed.get(CoursesService);
   });
 
-  it("should retrieve all courses", () => {});
+  it("should retrieve all courses", () => {
+    coursesService.findAllCourses().subscribe((courses) => {
+      expect(courses).toBeTruthy("No courses Returned");
+      expect(courses.length).toBe(12, "incorrect number of courses");
+      const course = courses.find((course) => course.id === 12);
+      expect(course.titles.description).toBe("Angular Testing Course");
+    });
+    const req = httpTestingController.expectOne("/api/courses");
+    expect(req.request.method).toEqual("GET");
+    req.flush({ payload: Object.values(COURSES) });
+  });
 });
